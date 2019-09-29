24b64c8cf673923d57db25cf51d65339a0cea8a7
Felix Palludan Hargreaves <hejfelix@gmail.com>
2019-09-29 10:42:16 +0200

    scalafmt

--- exercises/exercise1/src/main/scala/exercise1/SpecialFx.scala
+++ exercises/exercise1/src/main/scala/exercise1/SpecialFx.scala
@@ -1,8 +1,8 @@
 package exercise1
 
 object SpecialFx {
-  def delay[T](run: => T): SpecialFx[T]                        = Delay(() => run)
-  def pure[T](t: T): SpecialFx[T]                              = Pure(t)
+  def delay[T](run: => T): SpecialFx[T]                                   = Delay(() => run)
+  def pure[T](t: T): SpecialFx[T]                                         = Pure(t)
   def flatMap[T, U](et: SpecialFx[T])(f: T => SpecialFx[U]): SpecialFx[U] = FlatMapped(et, f)
 }
 
@@ -13,14 +13,14 @@ sealed trait SpecialFx[T] {
 }
 
 case class Delay[T](run: () => T) extends SpecialFx[T] {
-  override def unsafeRunSync: T = run()
+  override def unsafeRunSync: T                = run()
   override def map[U](f: T => U): SpecialFx[U] = Delay(() => f(run()))
 }
 case class Pure[T](t: T) extends SpecialFx[T] {
-  override def unsafeRunSync: T = t
+  override def unsafeRunSync: T                = t
   override def map[U](f: T => U): SpecialFx[U] = Pure(f(t))
 }
 case class FlatMapped[T, U](effect: SpecialFx[T], f: T => SpecialFx[U]) extends SpecialFx[U] {
-  override def unsafeRunSync: U = f(effect.unsafeRunSync).unsafeRunSync
+  override def unsafeRunSync: U                = f(effect.unsafeRunSync).unsafeRunSync
   override def map[A](g: U => A): SpecialFx[A] = FlatMapped(effect, f andThen (_.map(g)))
 }
