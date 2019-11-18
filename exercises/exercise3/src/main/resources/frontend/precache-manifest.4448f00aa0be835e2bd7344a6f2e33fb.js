ff8e315c9ad6ff89898bc4b923145531ede13799
Marco Martins <marco.martins@lego.com>
2019-11-18 13:07:55 +0100

    Update client with render html from server

---
+++ exercises/exercise3/src/main/resources/frontend/precache-manifest.4448f00aa0be835e2bd7344a6f2e33fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39cafb4ea976f2f1757fad5213798dd7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a80ff3e8e909ce88361c",
+    "url": "./static/css/main.bc62783b.chunk.css"
+  },
+  {
+    "revision": "0366e04f5c8b879e8e38",
+    "url": "./static/js/2.e8fb46e0.chunk.js"
+  },
+  {
+    "revision": "a80ff3e8e909ce88361c",
+    "url": "./static/js/main.f2822731.chunk.js"
+  },
+  {
+    "revision": "7307ae9592cad1bbe0aa",
+    "url": "./static/js/runtime-main.afe2f115.js"
+  }
+]);
